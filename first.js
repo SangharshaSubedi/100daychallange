377b1a8bb1a9b2f42d0dc9df25b5d40c04ded05b
bibeksangharsha1718 <bibeksangharsha1718@gmail.com>
2026-03-04 10:44:56 +0900

    day 2

--- first.js
+++ first.js
@@ -1,35 +1,34 @@
-const identity =
- {
-    fullName: "Mr. Struggle",
-    age: 30,
-    profession: "student"
-};
+// this is comment which is not visible in output. 
+console.log(" Hellor world its you man");
 
-console.log(identity.fullName);
+//operator in js, which is same in all kind of language.
+// Arithematic operators ie +, -, *, /, %
+let a = 10;
+let b = 20;
+let c = 15
+let d= 2;
+let e = 3;
+console.log("a =", a, "b =", b,"c =", c,"d =", d, "e =", e); 
+console.log("a + b"); // this will print a + b as it is in double quotes
+console.log(a + b);   // this will print 30 as it is the sum of a and b
+console.log(a * b);
+console.log(b % c);  // this will print 5 as it is the remainder of b divided by c
+console.log(d ** e); // this will print 8 as it is d raised to the power of e (2^3 = 8)
 
+//Unary operators ie ++, --, +, -
+number = 5;
+number++; // this will increment the value of number by 1
+console.log(number); // this will print 6
+number--; // this will decrement the value of number by 1
+console.log(number); // this will print 5 because it was decremented back to its original value
+console.log(-number); // this will print -5 as it is the negation of number and same œœrule with+
 
-const pie = 3.14;
-// Reassign let variable
-let stringType = "Hello";
-stringType = "Goodbye";  // it will work because we can reassign let variable
- 
-let numberType = 10;
-let booleanType = true;
-let undefinedType;
-let nullType = null;
+// assignment operators ie =, +=, -=, *=, /=, %=
+let x = 10;
+let y = 5;
+x += y; // this is equivalent to x = x + y, so x becomes 15
+console.log(x); // this will print 15
+x -= y; // this is equivalent to x = x - y, so x becomes 10
+console.log(x); // this will print 10
 
-// Object property access methods
-console.log(stringType);     // it print goodbye not hello
-console.log(typeof stringType);
-console.log(typeof numberType);
-
-
-
-
-const profile =
-{
-    username: "@struggle",
-    isFollow: true,
-    followers: 1000,
-    following: 500
-};
+//comaprison operators ie ==, !=, 
